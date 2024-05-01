c23284cd4781488088ac3cbd0fc963ae6aa64ea7
niall <niall@DESKTOP-CQMMV32>
2024-04-30 23:10:59 -0500

    Add project files.

---
+++ DataStructuresFinal/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace DataStructuresFinal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.locationTextBox = new System.Windows.Forms.TextBox();
+            this.rewardTextBox = new System.Windows.Forms.TextBox();
+            this.currentLocationTextBox = new System.Windows.Forms.TextBox();
+            this.timeDependencyTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(341, 95);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 51);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "display priority";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(114, 95);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(100, 22);
+            this.nameTextBox.TabIndex = 1;
+            // 
+            // locationTextBox
+            // 
+            this.locationTextBox.Location = new System.Drawing.Point(114, 123);
+            this.locationTextBox.Name = "locationTextBox";
+            this.locationTextBox.Size = new System.Drawing.Size(100, 22);
+            this.locationTextBox.TabIndex = 2;
+            // 
+            // rewardTextBox
+            // 
+            this.rewardTextBox.Location = new System.Drawing.Point(114, 151);
+            this.rewardTextBox.Name = "rewardTextBox";
+            this.rewardTextBox.Size = new System.Drawing.Size(100, 22);
+            this.rewardTextBox.TabIndex = 3;
+            // 
+            // currentLocationTextBox
+            // 
+            this.currentLocationTextBox.Location = new System.Drawing.Point(114, 207);
+            this.currentLocationTextBox.Name = "currentLocationTextBox";
+            this.currentLocationTextBox.Size = new System.Drawing.Size(100, 22);
+            this.currentLocationTextBox.TabIndex = 4;
+            // 
+            // timeDependencyTextBox
+            // 
+            this.timeDependencyTextBox.Location = new System.Drawing.Point(114, 179);
+            this.timeDependencyTextBox.Name = "timeDependencyTextBox";
+            this.timeDependencyTextBox.Size = new System.Drawing.Size(100, 22);
+            this.timeDependencyTextBox.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(64, 95);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(64, 123);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 16);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(64, 151);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 16);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(64, 179);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 16);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(64, 213);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 16);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "label5";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.timeDependencyTextBox);
+            this.Controls.Add(this.currentLocationTextBox);
+            this.Controls.Add(this.rewardTextBox);
+            this.Controls.Add(this.locationTextBox);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.TextBox locationTextBox;
+        private System.Windows.Forms.TextBox rewardTextBox;
+        private System.Windows.Forms.TextBox currentLocationTextBox;
+        private System.Windows.Forms.TextBox timeDependencyTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ DataStructuresFinal/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+class Quest
+{
+    public string Name { get; set; }
+    public string Location { get; set; }
+    public int Reward { get; set; }
+    public int TimeDependency { get; set; }
+
+    public Quest(string name, string location, int reward, int timeDependency)
+    {
+        Name = name;
+        Location = location;
+        Reward = reward;
+        TimeDependency = timeDependency;
+    }
+}
+
+class QuestManager
+{
+    private List<Quest> quests = new List<Quest>();
+    private List<string> locationsHeap = new List<string>();
+
+    public void AddQuest(string name, string location, int reward, int timeDependency)
+    {
+        Quest newQuest = new Quest(name, location, reward, timeDependency);
+        quests.Add(newQuest);
+
+        // Add location to the heap if not already present
+        if (!locationsHeap.Contains(location))
+        {
+            locationsHeap.Add(location);
+            HeapifyUp(locationsHeap, locationsHeap.Count - 1);
+        }
+    }
+
+    private void HeapifyUp(List<string> list, int index)
+    {
+        while (index > 0)
+        {
+            int parent = (index - 1) / 2;
+            if (string.Compare(list[index], list[parent]) < 0)
+                break;
+
+            string temp = list[index];
+            list[index] = list[parent];
+            list[parent] = temp;
+            index = parent;
+        }
+    }
+
+    public string GetQuestsByLocation(string currentLocation)
+    {
+        string result = $"Quests at {currentLocation}:\n";
+        foreach (var quest in quests.Where(q => q.Location == currentLocation))
+        {
+            result += $"  {quest.Name} - Reward: {quest.Reward}, Time Dependency: {quest.TimeDependency} days\n";
+        }
+        return result;
+    }
+}
+
+public class MainForm : Form
+{
+    private QuestManager questManager = new QuestManager();
+    private TextBox outputTextBox;
+    private TextBox currentLocationTextBox;
+
+    public MainForm()
+    {
+        Text = "Quest Manager";
+        Size = new System.Drawing.Size(400, 350);
+
+        // Input controls
+        Label nameLabel = new Label { Text = "Quest Name:", Location = new System.Drawing.Point(10, 10), AutoSize = true };
+        TextBox nameTextBox = new TextBox { Location = new System.Drawing.Point(120, 10), Width = 200 };
+        Label locationLabel = new Label { Text = "Location:", Location = new System.Drawing.Point(10, 40), AutoSize = true };
+        TextBox locationTextBox = new TextBox { Location = new System.Drawing.Point(120, 40), Width = 200 };
+        Label rewardLabel = new Label { Text = "Reward:", Location = new System.Drawing.Point(10, 70), AutoSize = true };
+        TextBox rewardTextBox = new TextBox { Location = new System.Drawing.Point(120, 70), Width = 200 };
+        Label timeDependencyLabel = new Label { Text = "Time Dependency:", Location = new System.Drawing.Point(10, 100), AutoSize = true };
+        TextBox timeDependencyTextBox = new TextBox { Location = new System.Drawing.Point(120, 100), Width = 200 };
+        Label currentLocationLabel = new Label { Text = "Current Location:", Location = new System.Drawing.Point(10, 130), AutoSize = true };
+        currentLocationTextBox = new TextBox { Location = new System.Drawing.Point(120, 130), Width = 200 };
+        Button addButton = new Button { Location = new System.Drawing.Point(10, 160), Text = "Add Quest" };
+        addButton.Click += (sender, e) =>
+        {
+            string name = nameTextBox.Text;
+            string location = locationTextBox.Text;
+            int reward;
+            int timeDependency;
+
+            try
+            {
+                reward = int.Parse(rewardTextBox.Text);
+                timeDependency = int.Parse(timeDependencyTextBox.Text);
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Invalid reward or time dependency. Please enter integer values.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            questManager.AddQuest(name, location, reward, timeDependency);
+            outputTextBox.Text = "Quest added successfully!";
+        };
+
+        // Output textbox
+        outputTextBox = new TextBox { Location = new System.Drawing.Point(10, 200), Width = 360, Height = 90, Multiline = true, ReadOnly = true };
+
+        // Buttons to view quests
+        Button viewByLocationButton = new Button { Location = new System.Drawing.Point(10, 300), Text = "View by Current Location" };
+        viewByLocationButton.Click += (sender, e) =>
+        {
+            string currentLocation = currentLocationTextBox.Text;
+            outputTextBox.Text = questManager.GetQuestsByLocation(currentLocation);
+        };
+
+        // Add controls to the form
+        Controls.AddRange(new Control[] { nameLabel, nameTextBox, locationLabel, locationTextBox, rewardLabel, rewardTextBox, timeDependencyLabel, timeDependencyTextBox, currentLocationLabel, currentLocationTextBox, addButton, outputTextBox, viewByLocationButton });
+    }
+
+    [STAThread]
+    static void Main()
+    {
+        Application.EnableVisualStyles();
+        Application.SetCompatibleTextRenderingDefault(false);
+        Application.Run(new MainForm());
+    }
+}
